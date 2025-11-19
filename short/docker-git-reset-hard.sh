23174e791b4cf9898503510a0013032d5ef14bde
Rafael Silva <rafaelsgoncalvesbh@gmail.com>
2025-11-19 06:05:39 -0300

    update docker-git-reset-hard.sh

--- short/docker-git-reset-hard.sh
+++ short/docker-git-reset-hard.sh
@@ -3,7 +3,6 @@ cd $HOME/content-proxy-web || true
 
 echo "Git Reset Hard (Update Local Force) rafaelsg-01/FlareSolverrWithExecuteScript..."
 
-ggit fetch --all
-git reset --hard origin/master
+git fetch --all && git reset --hard origin/master
 
 echo "Git Reset Hard (Update Local Force) atualizado."
