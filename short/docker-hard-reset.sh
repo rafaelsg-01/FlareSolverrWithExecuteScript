28403557d7aceaef656eeef52f6bd5b0883a35a6
Rafael Silva <rafaelsgoncalvesbh@gmail.com>
2025-11-19 15:53:17 -0300

    update docker-hard-reset.sh and docker-run.sh

--- short/docker-hard-reset.sh
+++ short/docker-hard-reset.sh
@@ -3,12 +3,6 @@ cd $HOME/content-proxy-web || true
 
 echo "Hard-reset container content-proxy-web-01..."
 
-read -r -p "HARD-RESET. Tem certeza? digite 'yes' para continuar: " response
-if [[ "$response" != "yes" ]]; then
-    echo "Operação cancelada."
-    exit 0
-fi
-
 bash ./short/docker-stop.sh
 bash ./short/docker-remove.sh
 bash ./short/docker-git-reset-hard.sh
