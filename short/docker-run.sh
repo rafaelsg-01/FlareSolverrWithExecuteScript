62cdf13a0ed4555ce2d95045a2a281d18ffae543
Rafael Silva <rafaelsgoncalvesbh@gmail.com>
2025-11-19 06:48:32 -0300

    update docker-run.sh

--- short/docker-run.sh
+++ short/docker-run.sh
@@ -5,7 +5,6 @@ echo "Run container content-proxy-web-01..."
 
 docker run -d \
     --name content-proxy-web-01 \
-    -p 8191:8191 \
     --env-file ./.env \
     -e LOG_LEVEL=info \
     --restart unless-stopped \
