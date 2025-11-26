ce343720771b987f9dbd8e00bd7e46ce195be3e3
Rafael Silva <rafaelsgoncalvesbh@gmail.com>
2025-11-26 02:27:44 -0300

    update utils.py

--- src/utils.py
+++ src/utils.py
@@ -226,12 +226,12 @@ def get_webdriver(proxy: dict = None, first_script: str = None, max_optimization
         options.add_argument('--disable-background-networking') # CULPADO CRÍTICO: Impede validação se a aba parecer "inativa"
         options.add_argument('--disable-renderer-backgrounding') # CULPADO: Mesmo do de cima
 
-        options.add_argument('--disable-default-apps')
-        options.add_argument('--disable-extensions')
+        # options.add_argument('--disable-default-apps')
+        # options.add_argument('--disable-extensions')
         # options.add_argument('--disable-sync')
-        options.add_argument('--disable-translate')
+        # options.add_argument('--disable-translate')
         # options.add_argument('--disable-popup-blocking')
-        options.add_argument('--disable-notifications')
+        # options.add_argument('--disable-notifications')
         # options.add_argument('--disable-domain-reliability')
         # options.add_argument('--disable-component-update')
 
